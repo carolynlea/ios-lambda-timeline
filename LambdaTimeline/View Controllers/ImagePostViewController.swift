5d648060b4ce88e02fea696b03cd1a62d58915aa
Carolyn Lea <carolynrlea@gmail.com>
2018-10-16 13:36:34 -0600

    added more filters

--- LambdaTimeline/View Controllers/ImagePostViewController.swift
+++ LambdaTimeline/View Controllers/ImagePostViewController.swift
@@ -13,11 +13,20 @@ class ImagePostViewController: ShiftableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        originalImage = imageView.image
         setImageViewHeight(with: 1.0)
         
         updateViews()
+        brightnessLabel.isHidden = true
+        contrastLabel.isHidden = true
+        saturationLabel.isHidden = true
+        intensityLabel.isHidden = true
+        brightnessSlider.isHidden = true
+        contrastSlider.isHidden = true
+        saturationSlider.isHidden = true
+        intensitySlider.isHidden = true
     }
+    
     
     func updateViews() {
         
@@ -30,7 +39,7 @@ class ImagePostViewController: ShiftableViewController {
         
         setImageViewHeight(with: originalImage.ratio)
         
-        imageView.image = image(byFiltering: originalImage)
+        //imageView.image = image(byFiltering: originalImage)
         
         chooseImageButton.setTitle("", for: [])
     }
@@ -115,33 +124,89 @@ class ImagePostViewController: ShiftableViewController {
     
     @IBAction func addFilter(_ sender: Any)
     {
-//        let ac = UIAlertController(title: "Choose filter", message: nil, preferredStyle: .actionSheet)
-//        ac.addAction(UIAlertAction(title: "CIBumpDistortion", style: .default, handler: setFilter))
-//        ac.addAction(UIAlertAction(title: "CIGaussianBlur", style: .default, handler: setFilter))
-//        ac.addAction(UIAlertAction(title: "CIPixellate", style: .default, handler: setFilter))
-//        ac.addAction(UIAlertAction(title: "CISepiaTone", style: .default, handler: setFilter))
-//        ac.addAction(UIAlertAction(title: "CITwirlDistortion", style: .default, handler: setFilter))
-//        ac.addAction(UIAlertAction(title: "CIUnsharpMask", style: .default, handler: setFilter))
-//        ac.addAction(UIAlertAction(title: "CIVignette", style: .default, handler: setFilter))
-//        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
-//        present(ac, animated: true)
+        let alertController = UIAlertController(title:"Choose Filter", message: "Do it!", preferredStyle:.actionSheet)
+        
+        let blur = UIAlertAction.init(title: "Gaussian Blur", style: .default) { (UIAlertAction) in
+            self.applyBlur()
+        }
+        let sepia = UIAlertAction.init(title: "Sepia", style: .default) { (UIAlertAction) in
+            self.applySepia()
+        }
+        let noir = UIAlertAction.init(title: "Noir", style: .default) { (UIAlertAction) in
+            self.applyNoirEffect()
+        }
+        let effectTransfer = UIAlertAction.init(title: "Photo Effect Process", style: .default) { (UIAlertAction) in
+            self.applyPhotoTransferEffect()
+        }
+        
+        let manual = UIAlertAction.init(title: "Manual Adjustment", style: .default) { (UIAlertAction) in
+            
+            self.brightnessLabel.isHidden = false
+            self.contrastLabel.isHidden = false
+            self.saturationLabel.isHidden = false
+            self.brightnessSlider.isHidden = false
+            self.contrastSlider.isHidden = false
+            self.saturationSlider.isHidden = false
+            self.applyManualEffect()
+        }
+        let cancel = UIAlertAction.init(title: "Cancel", style: .cancel)
+        
+        alertController.addAction(blur)
+        alertController.addAction(sepia)
+        alertController.addAction(noir)
+        alertController.addAction(effectTransfer)
+        alertController.addAction(manual)
+        alertController.addAction(cancel)
+        
+        self.present(alertController, animated: true, completion: nil)
     }
     
+    //"CIBumpDistortion", "CIPixellate", "CITwirlDistortion", "CIUnsharpMask", "CIVignette"
+    @IBAction func applySepia()
+    {
+        guard let image = imageView.image else {return}
+        imageView.image = applyFilterTo(image: image, filterEffect: Filter(filterName: "CISepiaTone", filterEffectValue: 0.90, filterEffectValueName: kCIInputIntensityKey))
+    }
+    func applyPhotoTransferEffect()
+    {
+        guard let image = imageView.image else {return}
+        imageView.image = applyFilterTo(image: image, filterEffect: Filter(filterName: "CIPhotoEffectProcess", filterEffectValue: nil, filterEffectValueName: nil))
+    }
+    func applyNoirEffect()
+    {
+        guard let image = imageView.image else {return}
+        imageView.image = applyFilterTo(image: image, filterEffect: Filter(filterName: "CIPhotoEffectNoir", filterEffectValue: nil, filterEffectValueName: nil))
+    }
+    func applyBlur()
+    {
+        guard let image = imageView.image else {return}
+        imageView.image = applyFilterTo(image: image, filterEffect: Filter(filterName: "CIGaussianBlur", filterEffectValue: 8.0, filterEffectValueName: kCIInputRadiusKey))
+    }
+    
+    func applyManualEffect()
+    {
+        
+        updateImage()
 
+    }
+    @IBAction func clearFilters(_ sender: Any)
+    {
+        imageView.image = originalImage
+    }
     
     @IBAction func brightness(_ sender: Any)
     {
-        updateViews()
+        updateImage()
     }
     
     @IBAction func contrast(_ sender: Any)
     {
-        updateViews()
+        updateImage()
     }
     
     @IBAction func saturation(_ sender: Any)
     {
-        updateViews()
+        updateImage()
     }
     
     @IBAction func intensity(_ sender: Any)
@@ -150,6 +215,38 @@ class ImagePostViewController: ShiftableViewController {
     }
     
     // MARK: Private
+    private func applyFilterTo(image: UIImage, filterEffect: Filter) -> UIImage?
+    {
+        guard let cgImage = image.cgImage else { return nil }
+        
+        let ciImage = CIImage(cgImage: cgImage)
+        let filter = CIFilter(name: filterEffect.filterName)
+        
+        filter?.setValue(ciImage, forKey: kCIInputImageKey)
+        
+        if let filterEffectValue = filterEffect.filterEffectValue,
+            let filterEffectValueName = filterEffect.filterEffectValueName
+        {
+            filter?.setValue(filterEffectValue, forKey: filterEffectValueName)
+        }
+        
+        var filteredImage: UIImage?
+        
+        if let output = filter?.value(forKey: kCIOutputImageKey) as? CIImage,
+            let cgiImageResult = context.createCGImage(output, from: output.extent)
+        {
+            filteredImage = UIImage(cgImage: cgiImageResult)
+        }
+        
+        return filteredImage
+    }
+    
+    private func updateImage()
+    {
+        guard let originalImage = originalImage else {return}
+        imageView?.image = image(byFiltering: originalImage)
+        
+    }
     
     private func image(byFiltering image: UIImage) -> UIImage?
     {
@@ -161,11 +258,14 @@ class ImagePostViewController: ShiftableViewController {
         filter.setValue(contrastSlider.value, forKey: kCIInputContrastKey)
         filter.setValue(saturationSlider.value, forKey: kCIInputSaturationKey)
 
-        sepiaFilter?.setValue(filter.outputImage, forKey: kCIInputImageKey)
-        sepiaFilter?.setValue(intensitySlider.value, forKey: kCIInputIntensityKey)
+//        sepiaFilter?.setValue(filter.outputImage, forKey: kCIInputImageKey)
+//        sepiaFilter?.setValue(intensitySlider.value, forKey: kCIInputIntensityKey)
+//
+//        splashFilter?.setValue(ciImage, forKey: kCIInputImageKey)
         
-        guard let outputCIImage = sepiaFilter?.outputImage,
-            let outputCGImage = context.createCGImage(outputCIImage, from: outputCIImage.extent) else {return nil}//extent is size of whole image, can also do just a part
+        guard let outputCIImage = filter.outputImage,
+        //guard let outputCIImage = splashFilter?.outputImage,
+            let outputCGImage = context.createCGImage(outputCIImage, from: outputCIImage.extent) else {return nil}
         return UIImage(cgImage: outputCGImage)
         
     }
@@ -185,6 +285,8 @@ class ImagePostViewController: ShiftableViewController {
     }
     private let filter = CIFilter(name: "CIColorControls")!
     private let sepiaFilter = CIFilter(name: "CISepiaTone")
+    private let splashFilter = CIFilter(name: "CICircleSplashDistortion")
+    
     private let context = CIContext(options: nil)
     private var originalImage: UIImage?
     {
@@ -205,6 +307,10 @@ class ImagePostViewController: ShiftableViewController {
     @IBOutlet var saturationSlider: UISlider!
     @IBOutlet var intensitySlider: UISlider!
     
+    @IBOutlet var brightnessLabel: UILabel!
+    @IBOutlet var contrastLabel: UILabel!
+    @IBOutlet var saturationLabel: UILabel!
+    @IBOutlet var intensityLabel: UILabel!
 }
 
 extension ImagePostViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
@@ -227,6 +333,19 @@ extension ImagePostViewController: UIImagePickerControllerDelegate, UINavigation
     }
 }
 
+struct Filter
+{
+    let filterName: String
+    var filterEffectValue: Any?
+    var filterEffectValueName: String?
+    
+    init(filterName: String, filterEffectValue: Any?, filterEffectValueName: String?)
+    {
+        self.filterName = filterName
+        self.filterEffectValue = filterEffectValue
+        self.filterEffectValueName = filterEffectValueName
+    }
+}
 //enum FilterType : String
 //{
 //    case Chrome = "CIPhotoEffectChrome"
